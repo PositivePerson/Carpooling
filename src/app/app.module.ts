c00ae85987c3f71c5d6278f4c2e0c57e9f29567d
Bartek <WonderfulLifeVision@gmail.com>
2020-06-20 13:05:10 +0200

    before removing defual typography

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { EconomyComponent } from './economy/economy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    EconomyComponent
   ],
   imports: [
     BrowserModule,
